0f0c9472004d46d20f74b9b9ec6f08bf1e1dbfc8
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-06-17 14:57:50 +0100

    Updating 0-list_databases

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-'''Task 10's module.
-'''
+"""Write a Python function that changes all topics of a
+school document based on the name:
+
+Prototype: def update_topics(mongo_collection, name, topics):
+mongo_collection will be the pymongo collection object
+name (string) will be the school name to update
+topics (list of strings) will be the list of topics approached
+in the school
+"""
+
+
+import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
-    '''Changes all topics of a collection's document based on the name.
-    '''
-    mongo_collection.update_many(
-        {'name': name},
-        {'$set': {'topics': topics}}
-    )
+    """changes all topics of a school document"""
+    return mongo_collection.update_many({"name": name},
+                                        {"$set": {"topics": topics}})
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-'''Task 11's module.
-'''
+"""Write a Python function that returns the list of school
+having a specific topic:
+
+Prototype: def schools_by_topic(mongo_collection, topic):
+mongo_collection will be the pymongo collection object
+topic (string) will be topic searched
+"""
+
+
+import pymongo
 
 
 def schools_by_topic(mongo_collection, topic):
-    '''Returns the list of school having a specific topic.
-    '''
-    topic_filter = {
-        'topics': {
-            '$elemMatch': {
-                '$eq': topic,
-            },
-        },
-    }
-    return [doc for doc in mongo_collection.find(topic_filter)]
+    """returns the list of school having a specific topic"""
+    return mongo_collection.find({"topics": topic})
